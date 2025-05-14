f7dc1b2d0199c0bf5db325dd5927e2935ec48004
Aum Sorathiya <mdsorathiya56@gmail.com>
2025-05-14 16:27:23 +0530

    Update Calculator.c

--- PR. 7 Calculator/Calculator.c
+++ PR. 7 Calculator/Calculator.c
@@ -2,48 +2,52 @@
 
 int cal()
 {
-	int choice, a, b;
+	int choice, a, b, i;
 	
-	printf("Enter the first Number: ");
+	printf("press 1 for +\n");
+    printf("press 2 for -\n");
+    printf("press 3 for *\n");
+    printf("press 4 for /\n");
+    printf("press 5 for %%\n");
+    printf("press 0 for for exit\n");
+
+	printf("Pless enter your choice: ");
+    scanf("%d",&choice);
+
+    printf("Enter the first Number: ");
     scanf("%d",&a);
     printf("Enter the second Number: ");
     scanf("%d",&b);
     
-    printf("press 1 for +\n");
-    printf("press 2 for -\n");
-    printf("press 3 for *\n");
-    printf("press 4 for /\n");
-    printf("press 5 for %\n");
-    printf("press 0 for for exit\n");
-    
-    printf("Pless enter your choice:");
-    scanf("%d",&choice);
-	
 	if(choice!=0){
 		
 		switch (choice)
 	{
-		case 1: printf("The Addition of your numbers %d & %d is: %d",a , b, a+b);
+		case 1: printf("The Addition of your numbers %d & %d is: %d\n",a , b, a+b);
 	    break;
-		case 2: printf("The Subration of your numbers %d & %d is: %d",a , b, a-b);
+		case 2: printf("The Subration of your numbers %d & %d is: %d\n",a , b, a-b);
 		break;
-		case 3: printf("The Multiplation of your numbers %d & %d is: %d",a , b, a*b);
+		case 3: printf("The Multiplation of your numbers %d & %d is: %d\n",a , b, a*b);
 		break;
-		case 4: printf("The Division of your numbers %d & %d is: %d",a , b, a/b);
+		case 4: printf("The Division of your numbers %d & %d is: %d\n",a , b, a/b);
 		break;
-		case 5: printf("The Rimender of your numbers is: %d", a, b, a%b);
+		case 5: printf("The Modulas of your numbers %d & %d is: %d\n", a, b, a%b);
+		break;
+		case 6: 
 		break;
 		default:
 		break;
     }
-    return choice;
+        return i==1;
 	}else{
-		return 0;
+		return i==5;
 	}
 	
 }
 
 int main()
 {
+	for(int i=1;i>=0;i--){
     cal();
+	}
 }
