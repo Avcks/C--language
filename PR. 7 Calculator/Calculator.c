878779b18b05f4d38bc95af78c109ef1269eed83
Aum Sorathiya <mdsorathiya56@gmail.com>
2025-05-14 16:50:43 +0530

    Update Calculator.c

--- PR. 7 Calculator/Calculator.c
+++ PR. 7 Calculator/Calculator.c
@@ -1,81 +1,102 @@
 #include <stdio.h>
 
-// User Defined Functions
-int add(int a, int b) {
-    return a + b;
+int add(int a, int b) 
+{
+    return a+b;
 }
 
-int subtract(int a, int b) {
-    return a - b;
+int s(int a, int b) 
+{
+    return a-b;
 }
 
-int multiply(int a, int b) {
-    return a * b;
+int m(int a, int b) 
+{
+    return a*b;
 }
 
-float divide(int a, int b) {
-    if (b != 0)
-        return (float)a / b;
-    else {
-        printf("Error: Division by zero!\n");
+float d(int a, int b) 
+{
+    if(b!=0){
+        return (float)a/b;
+    }else{
+        printf("Division by zero is not possiable\n");
         return 0;
     }
 }
 
-int modulus(int a, int b) {
-    if (b != 0)
-        return a % b;
-    else {
-        printf("Error: Modulus by zero!\n");
-        return 0;
-    }
+int mo(int a, int b) 
+{
+        return a%b;
 }
 
 int main() {
-    int choice, num1, num2;
+    int choice, x, y;
     
-    while (1) {
-        // Menu
+    while(1)
+	{
         printf("\nPress 1 for +\n");
         printf("Press 2 for -\n");
         printf("Press 3 for *\n");
         printf("Press 4 for /\n");
-        printf("Press 5 for %%\n");  // Use %% to print a single %
+        printf("Press 5 for %%\n"); 
         printf("Press 0 for the exit\n");
 
         printf("\nEnter your choice: ");
         scanf("%d", &choice);
 
-        if (choice == 0) {
-            printf("Exiting the program.\n");
+        if (choice==0) 
+		{
             break;
         }
-
         printf("Enter the first number: ");
-        scanf("%d", &num1);
+        scanf("%d", &x);
         printf("Enter the second number: ");
-        scanf("%d", &num2);
+        scanf("%d", &y);
 
-        switch (choice) {
+        switch (choice) 
+		{
             case 1:
-                printf("Addition of %d and %d is %d\n", num1, num2, add(num1, num2));
+                printf("Addition of %d and %d is %d\n", x, y, add(x, y));
                 break;
             case 2:
-                printf("Subtraction of %d and %d is %d\n", num1, num2, subtract(num1, num2));
+                printf("Subtraction of %d and %d is %d\n", x, y, s(x, y));
                 break;
             case 3:
-                printf("Multiplication of %d and %d is %d\n", num1, num2, multiply(num1, num2));
+                printf("Multiplication of %d and %d is %d\n", x, y, m(x, y));
                 break;
             case 4:
-                printf("Division of %d and %d is %.2f\n", num1, num2, divide(num1, num2));
+                printf("Division of %d and %d is %.2f\n", x, y, d(x, y));
                 break;
             case 5:
-                printf("Modulus of %d and %d is %d\n", num1, num2, modulus(num1, num2));
+                printf("Modulus of %d and %d is %d\n", x, y, m(x, y));
                 break;
             default:
-                printf("Invalid choice! Please try again.\n");
+                printf("choise not found.\n");
         }
     }
 
-   
 }
+
+/*
+Press 1 for +
+Press 2 for -
+Press 3 for *
+Press 4 for /
+Press 5 for %
+Press 0 for the exit
+
+Enter your choice: 1
+Enter the first number: 1
+Enter the second number: 14
+Addition of 1 and 14 is 15
+
+Press 1 for +
+Press 2 for -
+Press 3 for *
+Press 4 for /
+Press 5 for %
+Press 0 for the exit
+
+Enter your choice: 0
+*/
