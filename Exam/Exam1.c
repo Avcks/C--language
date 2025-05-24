a5f5134e52d031fbecbe401184eaba4dff984abf
Aum Sorathiya <mdsorathiya56@gmail.com>
2025-05-24 15:57:44 +0530

    Update Exam1.c

--- Exam/Exam1.c
+++ Exam/Exam1.c
@@ -6,39 +6,39 @@ int main()
 	scanf("%c",&ch);
 	switch(ch)
 	{
-		case 'a': 
-		    printf("Your character is Vowel.");
-		    break;
-		case 'e': 
-		    printf("Your character is Vowel.");
-		    break;
-		case 'i': 
-	        printf("Your character is Vowel.");
-	        break;
-		case 'o': 
-		    printf("Your character is Vowel.");
-		    break;
-		case 'u': 
-		    printf("Your character is Vowel.");
-		    break;
-		case 'A': 
-		    printf("Your character is Vowel.");
-		    break;
-		case 'E': 
-		    printf("Your character is Vowel.");
-		    break;
-		case 'I': 
-	        printf("Your character is Vowel.");
-	        break;
-		case 'O': 
-		    printf("Your character is Vowel.");
-		    break;
-		case 'U': 
-		    printf("Your character is Vowel.");
-		    break;
-		default:   
-		    printf("Your character is consonant.");
-		    break;
+	case 'a': 
+		printf("Your character is Vowel.");
+		break;
+	case 'e': 
+		printf("Your character is Vowel.");
+		break;
+	case 'i': 
+	    printf("Your character is Vowel.");
+	    break;
+	case 'o': 
+		printf("Your character is Vowel.");
+		break;
+	case 'u': 
+		printf("Your character is Vowel.");
+		break;
+	case 'A': 
+		printf("Your character is Vowel.");
+		break;
+	case 'E': 
+		printf("Your character is Vowel.");
+		break;
+	case 'I': 
+	    printf("Your character is Vowel.");
+	    break;
+	case 'O': 
+		printf("Your character is Vowel.");
+		break;
+	case 'U': 
+		printf("Your character is Vowel.");
+		break;
+	default:   
+		printf("Your character is consonant.");
+		break;
 	}
 }
 
